abda6eb46e60137ba092fc297e63a6484d49116e
acoramadan <laxmiwatimaani@gmail.com>
2024-09-13 23:17:02 +0800

    some changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,17 @@ package com.dicoding
 import com.dicoding.belajarDelegateClass.hehe
 import com.dicoding.Modul1.Kotlin_Fundamental
 import com.dicoding.ControlFlow.ControlFlow
+import com.dicoding.kotlin_oop.Manusia
 fun main() {
     val k = Kotlin_Fundamental()
     val z = ControlFlow()
-    print(k.tes("Ahmad"))
+    val manusia = Manusia()
+    println(k.tes("Ahmad"))
 
+    manusia.setNama("Mufli")
+    manusia.setUmur(20)
+    manusia.setTempatTinggal("Jl Tamalate setapak 22 no 16")
+    manusia.setJenisKelamin("Laki-Laki")
+
+    manusia.printManusia()
 }
