2de1c84f3d24419c65b926248e6c949ec23703aa
acoramadan <laxmiwatimaani@gmail.com>
2024-09-13 23:17:31 +0800

    some changes

---
+++ src/main/kotlin/kotlin_oop/Manusia.kt
@@ -0,0 +1,42 @@
+package com.dicoding.kotlin_oop
+
+class Manusia {
+    private var nama : String? = null
+    private var umur : Int? = null
+    private var tempatTingal : String? = null
+    private var jenisKelamin : String? = null
+
+    fun getNama() : String {
+        return nama ?: "";
+    }
+    fun setNama(nama : String?) {
+        this.nama = nama
+    }
+    fun getUmur() : Int {
+        return umur ?: 0
+    }
+    fun setUmur(umur : Int) {
+        this.umur = umur
+    }
+    fun getTempatTinggal() : String {
+        return tempatTingal ?: ""
+    }
+    fun setTempatTinggal(tempatTinggal : String) {
+        this.tempatTingal = tempatTinggal
+    }
+    fun getJenisKelamin() : String {
+        return jenisKelamin ?: ""
+    }
+    fun setJenisKelamin(jenisKelamin : String) {
+        this.jenisKelamin = jenisKelamin
+    }
+
+    fun printManusia() {
+        println("Nama           : ${getNama()}\n" +
+                "Umur           : ${getUmur()}\n" +
+                "Tempat Tinggal : ${getTempatTinggal()}\n" +
+                "Jenis Kelamin  : ${getJenisKelamin()}\n")
+    }
+
+}
+
