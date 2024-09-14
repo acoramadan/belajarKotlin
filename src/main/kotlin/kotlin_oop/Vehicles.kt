134f71c09e15f3a51c48090592ab22b9d8fa9a0d
acoramadan <laxmiwatimaani@gmail.com>
2024-09-14 14:38:17 +0800

    some changes

--- src/main/kotlin/kotlin_oop/Vehicles.kt
+++ src/main/kotlin/kotlin_oop/Vehicles.kt
@@ -1,4 +1,35 @@
 package com.dicoding.kotlin_oop
 
-class Vehicles {
+open class Vehicles(Pharga : Double, asalNegara : String) {
+    private var harga : Double = Pharga
+    fun getHarga() : Double{
+        return harga
+    }
+}
+
+private class Mobil(name : String?, kecepatan : Int?, merek : String?, harga : Double, asalNegara : String) : Vehicles(harga,asalNegara){
+
+    var name : String?
+    var kecepatan : Int?
+    var merek : String?
+    var isDibeli : Boolean
+
+    init{
+        this.name = if(name == null) "kosong" else name
+        this.kecepatan = if(kecepatan == null) 0 else kecepatan
+        this.merek = if(merek == null) "kosong" else merek
+        this.isDibeli = true
+    }
+
+    constructor(name : String?, kecepatan : Int?, merek : String?, isDibeli : Boolean,harga: Double,asalNegara: String)
+            : this(name,kecepatan,merek,harga,asalNegara) {
+        this.isDibeli = isDibeli
+    }
+}
+
+fun main(){
+    val mobil = Mobil("Avanza",150,"Honda",true,16500.00,"Jepang")
+    println(mobil.name)
+    print(mobil.getHarga())
+
 }
