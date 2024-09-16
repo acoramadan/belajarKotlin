c000a60b991b9767e5de19b26da5c4290b9cc5a0
acoramadan <laxmiwatimaani@gmail.com>
2024-09-16 10:58:53 +0800

    some changes

--- src/main/kotlin/CollectionOperations/Main.kt
+++ src/main/kotlin/CollectionOperations/Main.kt
@@ -36,6 +36,18 @@ class Main {
         sorted dan sortedDescending :
         akan mensort data secara ascending untu sorted,
         dan descending untuk sortedDescending
+
+        fold :
+        kita bisa dengan mudah melakukan perhitungan setiap nilai yang berada di dalam sebuah collection tanpa harus melakukan iterasi
+        item tersebut satu persatu menggunakan fungsi fold.
+
+        drop dan take:
+        kita bisa memangkas nilai kita berapa banyak yang kita mau
+        contohnya jika saya mempunyai list dengan nilai 1, 2, 3, 4, 5, 6,
+        ketika saya memberikan argumen drop 3. maka outputnya akan mulai dari
+        4, 5, 6
+
+
     */
 
 }
@@ -62,4 +74,28 @@ fun main() {
     println(listNumber.find{it == 1}) // output : 1
 
     println(listNumber.lastOrNull{it == 3 })
+
+    // fold, drop, dan take
+
+    /*
+        coddingan dibawah dapat dilihat bahwa saya memprint current dimana currentnya dimulai dari 2
+        dilihat dari argumen yang kita berikan.
+        kemudian akan memprint itemmnya, nah itemnya itu sendiri adalah List number
+        dan setiap iterasinya currentnya akan ditambah 1
+
+        terdapat juga FOLDRIGHT, cara kerjanya dimulai dari index terakhir
+    */
+    val number : List<Int> = listOf(3,4,1,2)
+    val fold = number.fold(2) { current,it ->
+        println("current : $current")
+        println("item $it")
+        println()
+        current+1
+    }
+    println("fold : $fold")
+
+    val drop = number.drop(2)
+    println("drop : $drop") // output 1, 2
+    println("dropLast : ${number.dropLast(2)}") // bedanya dia akan mendrop mulai dari belakang atau index belakang... output : 3,4
+
 }
