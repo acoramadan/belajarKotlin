38bf62f66e393b075b9895329352ef4aedbfe051
acoramadan <laxmiwatimaani@gmail.com>
2024-09-17 18:44:45 +0800

    some changes

--- src/main/kotlin/FunctionalProgramming/Recursion.kt
+++ src/main/kotlin/FunctionalProgramming/Recursion.kt
@@ -1,4 +1,22 @@
 package com.dicoding.FunctionalProgramming
 
+fun main() {
+    println(faktorial(4))
+}
+// recursion
+fun faktorial(number : Int) : Int {
+    if (number == 1) {
+        return number
+    }
+   return number * faktorial(number - 1)
+}
+// tail recursion!
+tailrec fun faktorial(n : Int, result : Int = 1) : Int {
+    val newResult = n * result
+    if (newResult == 1) {
+       return newResult
+    }
+    return faktorial(n - 1, newResult)
+}
 class Recursion {
 }
